f3415bdeb9bbaabab80f2344104f45e1420298bd
Leoneo <leoneo.vasques@gmail.com>
2025-06-02 16:02:34 -0300

    2.0

--- excluir.php
+++ excluir.php
@@ -3,9 +3,15 @@ include("conexao.php");
 include("log.php");
 $produto = "";
 $id = $_GET["id"];
+$sql = "SELECT produto FROM produtos WHERE id=$id";
+$result = $mysqli->query($sql);
+if ($result && $result->num_rows > 0){
+    $linha = $result->fetch_assoc();
+    $produto = $linha['produto'];
+}
 $sql = "DELETE FROM produtos WHERE id=$id";
 $result = $mysqli->query($sql);
-registrar_log($mysqli, 'EXCLUIR', 'produtos', 'excluir.php?id=$row[id]');
+registrar_log($mysqli, 'EXCLUIR', 'produtos', "$produto");
 header("location: aplicativo.php");
 exit;
 ?>
--- excluir_fornecedores.php
+++ excluir_fornecedores.php
@@ -1,7 +1,14 @@
 <?php
 include("conexao.php");
 include("log.php");
+$fornecedor = "";
 $id = $_GET["id"];
+$sql = "SELECT fornecedor FROM fornecedores WHERE id=$id";
+$result = $mysqli->query($sql);
+if ($result && $result->num_rows > 0){
+    $linha = $result->fetch_assoc();
+    $fornecedor = $linha['fornecedor'];
+}
 $sql = "DELETE FROM fornecedores WHERE id=$id";
 $result = $mysqli->query($sql);
 registrar_log($mysqli, 'EXCLUIR', 'fornecedores', "$fornecedor");
